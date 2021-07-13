42766b8a3320fe2a7bede602ce5eda412cca05b7
Alexey Goncharov <80ka96@gmail.com>
2021-07-13 19:37:08 +0300

    add templete engine

---
+++ app/views/template/base/header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Apple Shop</title>
+  <link rel="shortcut icon" href="/public/img/favicon.ico" type="image/x-icon">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Sniglet:wght@400;800&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/public/css/main.css">
+  <?php foreach ($styles as $style): ?>
+    <link rel="stylesheet" href="/public/css/<?= $style ?>.css">
+  <?php endforeach; ?>
+  <script src="https://use.fontawesome.com/86cb787e9f.js"></script>
+</head>
+<body>
+  <header class="header">
+    <div class="header__wrapper container">
+      <div class="logo"><a href="index.php"><img src="/public/img/logo.png" alt=""></a></div>
+      <nav class="nav-bar">
+        <div class="site-navigation">
+          <ul>
+            <li><a href="products.php">Products</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div class="user-navigation">
+          <ul>
+            <li><a href="#"><i class="fa fa-search fa-lg"></i></a></li>
+            <li><a href="login.php"><i class="fa fa-user fa-lg"></i></a></li>
+            <li><a href="cart.php"><i class="fa fa-shopping-cart fa-lg"></i></a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
---
+++ app/views/template/pages/products.php
@@ -0,0 +1,160 @@
+<main class="main main--products-page">
+  <section class="product-section">
+    <div class="container">
+      <section class="products products--all">
+        <nav>
+          <ul class="breadcrumb">
+            <li><a href="index.php">Home</a></li>
+            <li>Products</li>
+          </ul>
+        </nav>
+        <h2>All item</h2>
+        <ul class="products-list">
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+          <li class="product-item">
+            <a href="product.php">
+              <div class="product-item__image">
+                <img src="/public/img/item_not_found.png" alt="">
+              </div>
+              <div class="product-item__text">
+                <p class="product-item__name">Item Name</p>
+                <p class="product-item__price">20 $</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section class="pagination-section">
+        <ul class="pagination">
+          <li><a href="#" class="prev">< Prev</a></li>
+          <li class="page-number active"><a href="#">1</a></li>
+          <li class="page-number"><a href="#">2</a></li>
+          <li class="page-number"><a href="#">3</a></li>
+          <li class="page-number"><a href="#">4</a></li>
+          <li class="page-number"><a href="#">5</a></li>
+          <li class="page-number"><a href="#">6</a></li>
+          <li><a href="#" class="next">Next ></a></li>
+        </ul>
+      </section>
+    </div>
+  </section>
