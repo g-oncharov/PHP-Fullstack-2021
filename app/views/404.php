55c3a9bc554aa8447c99e17dd92182ca2e883922
Alexey Goncharov <80ka96@gmail.com>
2021-07-21 17:41:18 +0300

    add 404 page with css

--- app/views/404.php
+++ app/views/404.php
@@ -1,2 +1,31 @@
-<?php
-echo '404 Not Found';
+<main class="main inner-main main--e404">
+    <div class="container">
+        <nav class="breadcrumb-nav">
+            <ul class="breadcrumb">
+                <li><a href="/">Home</a></li>
+                <li>404</li>
+            </ul>
+        </nav>
+        <section class="e404-section">
+            <h2>404</h2>
+            <div class="e404-section__wrapper">
+                <div class="oops-block">
+                    <h2>Oops!</h2>
+                </div>
+                <div class="text-block">
+                    <h3>Sorry, this page could not be found!</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Ad aliquid beatae
+                        eaque hic labore nam nemo unde!
+                        Accusantium ad cupiditate doloribus
+                        ducimus esse fugiat incidunt ipsa iste,
+                        labore laboriosam laborum nam perferendis,
+                        quaerat sed sit temporibus totam ullam
+                        veniam voluptatibus?
+                    </p>
+                    <a href="/">Go back to Homepage</a>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
