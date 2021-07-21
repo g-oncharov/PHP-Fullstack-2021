08bb4fc9ba3040471f782efdb358ff74fe2c40ad
Alexey Goncharov <80ka96@gmail.com>
2021-07-21 16:59:23 +0300

    add condition in header

--- app/template/base/header.php
+++ app/template/base/header.php
@@ -13,30 +13,32 @@
   <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Sniglet:wght@400;800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="/public/css/main.css">
-  <?php foreach ($styles as $style): ?>
-    <link rel="stylesheet" href="/public/css/<?= $style ?>.css">
-  <?php endforeach; ?>
+    <?php if (!empty($styles)): ?>
+        <?php foreach ($styles as $style): ?>
+        <link rel="stylesheet" href="/public/css/<?= $style ?>.css">
+        <?php endforeach; ?>
+    <?php endif; ?>
   <script src="https://use.fontawesome.com/86cb787e9f.js"></script>
 </head>
 <body>
-  <header class="header">
-    <div class="header__wrapper container">
-      <div class="logo"><a href="/"><img src="/public/img/logo.png" alt=""></a></div>
-      <nav class="nav-bar">
-        <div class="site-navigation">
-          <ul>
-            <li><a href="products">Products</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </div>
-        <div class="user-navigation">
-          <ul>
-            <li><a href="#"><i class="fa fa-search fa-lg"></i></a></li>
-            <li><a href="login"><i class="fa fa-user fa-lg"></i></a></li>
-            <li><a href="cart"><i class="fa fa-shopping-cart fa-lg"></i></a></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  </header>
+<header class="header">
+  <div class="header__wrapper container">
+    <div class="logo"><a href="/"><img src="/public/img/logo.png" alt=""></a></div>
+    <nav class="nav-bar">
+      <div class="site-navigation">
+        <ul>
+          <li><a href="products">Products</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div class="user-navigation">
+        <ul>
+          <li><a href="#"><i class="fa fa-search fa-lg"></i></a></li>
+          <li><a href="login"><i class="fa fa-user fa-lg"></i></a></li>
+          <li><a href="cart"><i class="fa fa-shopping-cart fa-lg"></i></a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</header>
