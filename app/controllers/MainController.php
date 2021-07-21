dc79140dc5d73f2e5c979948d6e8a4f7ff8698df
Alexey Goncharov <80ka96@gmail.com>
2021-07-21 16:27:39 +0300

    add authorization using a form

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,12 +3,16 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/app/models/Product.php');
 class MainController extends Controller {
     protected $model;
     public $view;
+    public $authentication;
+    public $session;
 
     public function __construct()
     {
         $productList = include_once($_SERVER['DOCUMENT_ROOT'] . '/app/helpers/products-list.php');
-        $this->model = new Product($productList);;
+        $this->model = new Product($productList);
         $this->view = new View;
+        $this->authentication = new Authentication;
+        $this->session = new Session;
     }
 
     public function actionIndex() {
@@ -16,27 +20,37 @@ class MainController extends Controller {
         $this->view->render('index', $params);
     }
     public function actionProducts() {
-        $params = ['styles' => ['products', 'products-section', 'breadcrumb-section', 'pagination-section']];
+        $params = ['styles' => ['products', 'products-section', 'pagination-section']];
         $this->view->render('products', $params);
     }
 
     public function actionProduct() {
-        $params = ['styles' => ['product', 'breadcrumb-section']];
+        $params = ['styles' => ['product']];
         $this->view->render('product', $params);
     }
 
     public function actionLogin() {
-        $params = ['styles' => ['login', 'breadcrumb-section']];
+        $params = ['styles' => ['login']];
         $this->view->render('login', $params);
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            extract($_POST);
+            $this->authentication->auth($name,$pass);
+            if ($this->authentication->isAuth()) {
+                var_dump('authorized');
+                $this->session->set("auth", true);
+                $this->session->set("login", $name);
+            }
+        }
     }
 
     public function actionCart() {
-        $params = ['styles' => ['cart', 'breadcrumb-section']];
+        $params = ['styles' => ['cart']];
         $this->view->render('cart', $params);
     }
 
-    public function actionError404() {
-        $params = [];
+    public function action404() {
+        $params = ['styles' => ['404']];
         $this->view->render('404', $params);
     }
 
