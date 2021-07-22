0338d1f454f8fd4f2f2d1c61886762079f548666
Alexey Goncharov <80ka96@gmail.com>
2021-07-22 13:06:39 +0300

    add signin page

---
+++ app/views/signin.php
@@ -0,0 +1,49 @@
+<main class="main inner-main main--product-page">
+  <div class="container">
+    <nav class="breadcrumb-nav">
+      <ul class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li>Cabinet</li>
+      </ul>
+    </nav>
+    <section class="signin-section">
+      <h2>Signin</h2>
+      <form method="post">
+        <div class="signin-section__input-list">
+          <div class="signin-section__input-item">
+            <label for="fname"><b>First name</b></label>
+            <input type="text" name="fname" class="input" id="fname" required>
+          </div>
+          <div class="signin-section__input-item">
+            <label for="lname"><b>Last name</b></label>
+            <input type="text" name="lname" class="input" id="lname" required>
+          </div>
+          <div class="signin-section__input-item">
+            <label for="email"><b>Email</b></label>
+            <input type="text" name="email" class="input" id="email" required>
+          </div>
+          <div class="signin-section__input-item">
+            <label for="login"><b>Login</b></label>
+            <input type="text" name="login" class="input" id="login" required>
+          </div>
+          <div class="signin-section__input-item">
+            <label for="pass"><b>Password</b></label>
+            <input type="password" name="pass" class="input" id="pass" required>
+          </div>
+          <div class="signin-section__input-item">
+            <label for="pass2"><b>Confirm Password</b></label>
+            <input type="password" name="pass2" class="input" id="pass2" required>
+          </div>
+        </div>
+          <div class="signin-section__footer">
+            <button type="submit" class="btn btn-buy">Sign in</button>
+            <a href="signin" class="login-link">I have an account</a>
+          </div>
+        </div>
+
+        <div class="button-wrapper" style="background-color:#f1f1f1">
+        </div>
+      </form>
+    </section>
+  </div>
+</main>
