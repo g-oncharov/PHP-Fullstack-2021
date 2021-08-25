31709c9ae8cec568f5441a8087bff8c39cb9dbc1
Alexey Goncharov <80ka96@gmail.com>
2021-08-25 22:03:54 +0300

    uncomment pagination

--- App/views/category.php
+++ App/views/category.php
@@ -13,17 +13,9 @@
           <li class="product-item product-item--loader"><div class="loader">Loading...</div></li>
           </ul>
       </section>
-<!--      <section class="pagination-section">-->
-<!--        <ul class="pagination">-->
-<!--          <li><a href="#" class="prev">< Prev</a></li>-->
-<!--          <li class="page-number active"><a href="#">1</a></li>-->
-<!--          <li class="page-number"><a href="#">2</a></li>-->
-<!--          <li class="page-number"><a href="#">3</a></li>-->
-<!--          <li class="page-number"><a href="#">4</a></li>-->
-<!--          <li class="page-number"><a href="#">5</a></li>-->
-<!--          <li class="page-number"><a href="#">6</a></li>-->
-<!--          <li><a href="#" class="next">Next ></a></li>-->
-<!--        </ul>-->
-<!--      </section>-->
+      <section class="pagination-section">
+        <ul class="pagination">
+        </ul>
+      </section>
     </div>
   </section>
