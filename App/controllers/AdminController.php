290c346f4bdb43be6d6928f0a6abef04baa2b90c
Alexey Goncharov <80ka96@gmail.com>
2021-08-26 15:06:58 +0300

    add update method

--- App/controllers/AdminController.php
+++ App/controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace Controller;
 
 use Framework\Controller\Controller;
+use Framework\Session\Session;
 use Model\Categories;
 use Model\Product;
 use Model\Admin;
@@ -18,12 +19,16 @@ class AdminController extends Controller
     /** @var Admin */
     private Admin $admin;
 
+    /** @var Session */
+    protected Session $session;
+
     public function __construct()
     {
         parent::__construct();
         $this->categories = new Categories();
         $this->product = new Product();
         $this->admin = new Admin();
+        $this->session = new Session();
     }
 
     public function add()
@@ -49,6 +54,51 @@ class AdminController extends Controller
             ];
 
             $this->view->render('add', $params);
+        } else {
+            $this->url->goToHomePage();
+        }
+    }
+
+    public function update()
+    {
+        if ($this->auth->isAdmin()) {
+            $idProduct = (int) $this->url->getLastPartUrl();
+            $product = $this->product->findById($idProduct);
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $data = $_POST;
+                $data['id'] = $idProduct;
+                $data['image'] = $product->getImage();
+                if ($this->admin->update($data)) {
+                    $this->url->goToPage("product/$idProduct");
+                }
+            }
+            $categories = $this->categories->findAll();
+            $category_id = $product->getCategoryId();
+
+            for ($i = 0; $i < count($categories); $i++) {
+                if ($categories[$i]->getId() === $category_id) {
+                    unset($categories[$i]);
+                }
+            }
+
+            $error = $this->admin->getError();
+
+            $styles = ['orders', 'add'];
+            $scripts = ['add'];
+            $params = [
+                'categories' => $categories,
+                'error' => $error,
+                'title' => $product->getTitle(),
+                'description' => $product->getDescription(),
+                'price' => $product->getPrice(),
+                'category' => $product->getCategory(),
+                'categoryId' => $category_id,
+                'image' => $product->getImage(),
+                'styles' => $styles,
+                'scripts' => $scripts,
+            ];
+
+            $this->view->render('update', $params);
         } else {
             $this->url->goToHomePage();
         }
